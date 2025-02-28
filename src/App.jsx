c1749a66014504e014d2d63ca09cacc5305d802b
Asha Deepthi Modalavalasa <202040257asha@gmail.com>
2025-02-28 15:58:36 +0530

    magic square

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Level1 from './components/Level1.jsx'
 import Checkpoints from './components/Checkpoints.jsx'
 import Login from './components/Login.jsx'
 import Leaderboard_level1 from './components/Leaderboard_level1.jsx'
+import MagicSquare from './components/MagicSquare.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="/level1" element={<Level1 />} />
         <Route path="/checkpoints" element={<Checkpoints />} />
         <Route path="/leaderboard" element={<Leaderboard_level1 />} />
+        <Route path="/magicSquare" element={<MagicSquare />} />
+       
       </Routes>
     </Router>
   )
